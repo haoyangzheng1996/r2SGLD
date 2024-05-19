cdda6d3be0f29fad581af0f8896a347a5e88adb9
Haoyang Zheng <38525155+haoyangzheng1996@users.noreply.github.com>
2024-05-19 07:18:21 -0400

    Update and rename main_sample.sh to run_sample.sh

--- multimodal_simulation/run_sample.sh
+++ multimodal_simulation/run_sample.sh
@@ -1,7 +1,7 @@
 clear
 
 # plot kl-divergence
-python run_flower.py --num_points 100 --M 5 --warm_up 1000 --plot_after 500 --n_epoch 100000
+python plot_flower.py --num_points 100 --M 5 --warm_up 1000 --plot_after 500 --n_epoch 100000
 
 # multi-modal simulation
 python sample_with_bound.py --optimizer 'sgld' --lr 5e-4 --num_points 1000 --save_after 10 --n_epoch 500000 --domain_type flower --if_include_domain True --radius 2
